fe096d20d38a728b12173ebc00f8b76a19b93a82
QuentinGP <quentgeoffpro@yahoo.com>
2024-09-19 16:39:27 +0200

    [fix] Dashboard  scss fixé, reste bien mettre en place

--- src/components/ChangeCurrency/ChangeCurrency.tsx
+++ src/components/ChangeCurrency/ChangeCurrency.tsx
@@ -7,35 +7,36 @@ import "./ChangeCurrency.scss";
 
 const ChangeCurrency: React.FC = () => {
     return (
-        <div className="transfert">
-            <div>
+        <div className="transfertWallet">
+            <div className="wallet">
                 <div>
                     <strong>Portefeuille</strong>
                     <p>MAJ à l’instant</p>
                 </div>
                 <div>
                     <span>10</span>
-                    <img src={Coin} alt="Pièce CAT²" />
+                    <img src={Coin} alt="Pièce CAT²" className="piece" />
                 </div>
             </div>
             <hr />
-            <div>
+            <div className="transfert">
                 <div>
-                    <div></div>
+                    <div className="flagUs"></div>
                     <select>
                         <option>US dollars</option>
                         <option>Euros</option>
                     </select>
                 </div>
                 <div>
-                    <span>320$</span>
+                    <span className="devise">320$</span>
                     <ArrowRight size={16} />
-                    <span>
-                        320 <img src={Coin} alt="Pièce CAT²" />
+                    <span className="deviseCat">
+                        320{" "}
+                        <img src={Coin} alt="Pièce CAT²" className="piece" />
                     </span>
                 </div>
             </div>
-            <div>
+            <div className="send">
                 <Button variant="primary" rounded={false}>
                     Envoyer un token
                 </Button>
