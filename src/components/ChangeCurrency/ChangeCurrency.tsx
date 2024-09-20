71921e53165abb02e2db8cdf2f9cbaeeb34eaf3b
QuentinGP <quentgeoffpro@yahoo.com>
2024-09-20 11:10:35 +0200

    Modals Intégration OK

--- src/components/ChangeCurrency/ChangeCurrency.tsx
+++ src/components/ChangeCurrency/ChangeCurrency.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { Button } from "../Button";
 import Coin from "../../assets/images/png/coin/coin-cat__full.png";
-import { ArrowRight, ArrowUpRight, Gear } from "phosphor-react";
+import {
+    ArrowRight,
+    ArrowUpRight,
+    Gear,
+    X,
+    MagnifyingGlass,
+} from "phosphor-react";
 import { Autocomplete, TextField } from "@mui/material";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
@@ -18,12 +24,31 @@ const ChangeCurrency: React.FC = () => {
     const [openFirstModal, setOpenFirstModal] = useState(false);
     const [openSecondModal, setOpenSecondModal] = useState(false);
     const [selectedCurrency, setSelectedCurrency] = useState(currencies[0]);
+    const [tokenAmount, setTokenAmount] = useState(0);
 
     const handleOpenFirstModal = () => setOpenFirstModal(true);
-    const handleCloseFirstModal = () => setOpenFirstModal(false);
+
+    // Ajout d'un délai de 300ms avant la fermeture
+    const handleCloseFirstModalWithDelay = () => {
+        setTimeout(() => {
+            setOpenFirstModal(false);
+        }, 300); // Délai de 300ms
+    };
 
     const handleOpenSecondModal = () => setOpenSecondModal(true);
     const handleCloseSecondModal = () => setOpenSecondModal(false);
+
+    const incrementToken = () => {
+        setTokenAmount((prev) => prev + 1);
+    };
+    const decrementToken = () => {
+        setTokenAmount((prev) => (prev > 0 ? prev - 1 : 0));
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(e.target.value, 10);
+        setTokenAmount(isNaN(value) ? 0 : value);
+    };
 
     return (
         <div className="transfert-wallet">
@@ -77,31 +102,53 @@ const ChangeCurrency: React.FC = () => {
                     <ArrowUpRight size={16} />
                     Envoyer un token
                 </Button>
-
                 <Modal
                     keepMounted
                     open={openFirstModal}
-                    onClose={handleCloseFirstModal}
+                    onClose={handleCloseFirstModalWithDelay}
                     aria-labelledby="first-modal-title"
                     aria-describedby="first-modal-description"
                 >
                     <Box className="Modal">
                         <p>Gérez vos Tokens</p>
                         <div className="inputToken">
-                            <img
-                                src={Coin}
-                                alt="Pièce CAT²"
-                                className="piece"
-                            />
-                            <p>CAT²</p>
+                            <div className="Modal-head">
+                                <img
+                                    src={Coin}
+                                    alt="Pièce CAT²"
+                                    className="piece"
+                                />
+                                <p>CAT²</p>
+                            </div>
+                            <div className="ModalNumber">
+                                <span onClick={decrementToken}>-</span>{" "}
+                                <input
+                                    type="number"
+                                    value={tokenAmount}
+                                    onChange={handleInputChange}
+                                    placeholder="0"
+                                />
+                                <span onClick={incrementToken}>+</span>{" "}
+                            </div>
                         </div>
-                        <input type="number" />
-                        <Button variant="primary" rounded={false}>
-                            Ajouter
-                        </Button>
-                        <Button variant="secondary" rounded={false}>
-                            Supprimer
-                        </Button>
+                        <div className="PopinSend">
+                            <Button variant="primary" rounded={false}>
+                                Ajouter
+                            </Button>
+                            <Button
+                                variant="secondary"
+                                rounded={false}
+                                onClick={() => setTokenAmount(0)}
+                            >
+                                Supprimer
+                            </Button>
+                        </div>
+                        <span
+                            className="close"
+                            onClick={handleCloseFirstModalWithDelay}
+                        >
+                            <X size={16} color="#FCFEFF" />
+                        </span>
                     </Box>
                 </Modal>
 
@@ -121,7 +168,55 @@ const ChangeCurrency: React.FC = () => {
                     aria-labelledby="second-modal-title"
                     aria-describedby="second-modal-description"
                 >
-                    <Box className="Modal"></Box>
+                    <Box className="Modal">
+                        <div className="Search">
+                            <span>
+                                <MagnifyingGlass size={16} color="#FCFEFF" />
+                            </span>
+                            <input
+                                type="search"
+                                placeholder="Recherche un contact..."
+                            />
+                        </div>
+                        <div className="inputToken">
+                            <div className="Modal-head">
+                                <img
+                                    src={Coin}
+                                    alt="Pièce CAT²"
+                                    className="piece"
+                                />
+                                <p>CAT²</p>
+                            </div>
+                            <div className="ModalNumber">
+                                <span onClick={decrementToken}>-</span>{" "}
+                                <input
+                                    type="number"
+                                    value={tokenAmount}
+                                    onChange={handleInputChange}
+                                    placeholder="0"
+                                />
+                                <span onClick={incrementToken}>+</span>{" "}
+                            </div>
+                        </div>
+                        <div className="PopinSend">
+                            <Button variant="primary" rounded={false}>
+                                Ajouter
+                            </Button>
+                            <Button
+                                variant="secondary"
+                                rounded={false}
+                                onClick={() => setTokenAmount(0)}
+                            >
+                                Supprimer
+                            </Button>
+                        </div>
+                        <span
+                            className="close"
+                            onClick={handleCloseFirstModalWithDelay}
+                        >
+                            <X size={16} color="#FCFEFF" />
+                        </span>
+                    </Box>
                 </Modal>
             </div>
         </div>
